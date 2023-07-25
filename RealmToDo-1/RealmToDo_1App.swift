dc27d2eaaf36825a2da61e5ee46dcf13c8e58c50
Noah Pope <npope@tutanota.com>
2023-07-25 06:46:51 -0400

    Initial Commit

---
+++ RealmToDo-1/RealmToDo_1App.swift
@@ -0,0 +1,17 @@
+//
+//  RealmToDo_1App.swift
+//  RealmToDo-1
+//
+//  Created by Noah Pope on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RealmToDo_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
