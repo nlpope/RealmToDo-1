65ae0ec77a6e218dec66561d9ced377438bfd691
Noah Pope <npope@tutanota.com>
2023-07-26 07:49:20 -0400

    Adding ToDo model, and navigation view & textfield w working addition button

--- RealmToDo-1/Models/ToDo.swift
+++ RealmToDo-1/Models/ToDo.swift
@@ -8,10 +8,32 @@
 import Foundation
 import RealmSwift
 
-class ToDo: Object {
-     
+class ToDo: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    //can only have 1 primary key per class
     @Persisted var name: String
     @Persisted var completed = false
+    @Persisted var urgency: Urgency = .neutral
     //@Persisted guarantees a default value, so no errors
     //upsert = inserting an object that doesnt exist or updating one that does
+    
+    enum Urgency: Int, PersistableEnum {
+        case trivial, neutral, urgent
+        
+        var text: String {
+            switch self {
+            case .trivial:
+                return "Trivial"
+            case .neutral:
+                return "Neutral"
+            case .urgent:
+                return "Urgent"
+            }
+        }
+    }
+    
+    convenience init(name: String) {
+        self.init()
+        self.name = name
+    }
 }
---
+++ RealmToDo-1/Views/ToDoListView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  RealmToDo-1
+//
+//  Created by Noah Pope on 7/25/23.
+//
+
+/**
+ initial commit
+ studying realm w stewart lynch
+ */
+
+import SwiftUI
+import RealmSwift
+
+struct ToDoListView: View {
+    @ObservedResults(ToDo.self) var toDos
+    @State private var name = ""
+    @FocusState private var focus: Bool?
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("", text: $name)
+                        .focused($focus, equals: true)
+                        .textFieldStyle(.roundedBorder)
+                    Spacer()
+                    Button {
+                        let newToDo = ToDo(name: name)
+                        $toDos.append(newToDo)
+                        name = ""
+                        focus = nil
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(name.isEmpty)
+                }
+                .padding()
+                List() {
+                    ForEach(toDos) { toDo in
+                        Text(toDo.name)
+                    }
+                    .listRowSeparator(.hidden)
+                }
+                .listStyle(.plain)
+            }
+            .navigationTitle("Realm ToDos")
+        }
+    }
+}
+
+struct ToDoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListView()
+    }
+}
